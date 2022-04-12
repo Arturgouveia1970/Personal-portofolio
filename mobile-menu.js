d50fa58793022c1ccf026aea4313ec793315991e
Arturgouveia1970 <artgouveiag@gmail.com>
2022-04-12 10:54:33 +0100

    linters errors corrections

--- mobile-menu.js
+++ mobile-menu.js
@@ -4,15 +4,13 @@ const nav = document.querySelector('.nav-list');
 const mobileBtnExit = document.getElementById('mobile-exit');
 
 mobileBtn.addEventListener('click', () => {
-    nav.style.display="block";
-
+  nav.style.display = 'block';
 });
 
 mobileBtnExit.addEventListener('click', () => {
-    nav.style.display="none";
+  nav.style.display = 'none';
 })
 
 nav.addEventListener('click', () => {
-    nav.style.display="none";
+  nav.style.display = 'none';
 });
-
