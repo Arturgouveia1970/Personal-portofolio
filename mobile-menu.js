b122115a48fd3b7ea68b11c9a44b6b3033bd66de
Arturgouveia1970 <artgouveiag@gmail.com>
2022-04-12 10:41:48 +0100

    reorganized mobile-menu

--- mobile-menu.js
+++ mobile-menu.js
@@ -4,14 +4,15 @@ const nav = document.querySelector('.nav-list');
 const mobileBtnExit = document.getElementById('mobile-exit');
 
 mobileBtn.addEventListener('click', () => {
-    nav.classList.add('menu-btn');
+    nav.style.display="block";
+
 });
 
 mobileBtnExit.addEventListener('click', () => {
-    nav.classList.remove('menu-btn')
+    nav.style.display="none";
 })
 
 nav.addEventListener('click', () => {
-    nav.classList.remove('menu-btn');
+    nav.style.display="none";
 });
 
